6d1a52d87ef61ecba57c653a4ec8aa576af23de1
yohei <yohei@e-ml.mobi>
2016-08-19 13:47:17 +0000

    add sign sign up and user detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+    before_save { self.email = self.email.downcase}
+    validates :name, presence: true, length: { maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+    
+    has_secure_password
 end
+
+# 2行目で、データの保存前にメールアドレスのアルファベットを小文字にする。
+#before_saveはコールバックと呼ばれ、データの保存が行われる前に指定したブロックまたはメソッドの処理が実行されます。
+#•4行目のVALID_EMAIL_REGEXでメールアドレスの正規表現パターンを定義しています。（正規表現についてはここでは理解しなくも大丈夫です。）
+
+#•5〜7行目で、emailは空でなく、255文字以内で、VALID_EMAIL_REGEXのパターンに一致し、他と異なるようにバリデーションを行っています。
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20160819104531) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_daigest"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
